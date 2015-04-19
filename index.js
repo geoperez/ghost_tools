f15211c8f5b92f0434e46458f5972f28a29cdc64
Geo Perez <geovanni.perez@gmail.com>
2015-04-19 13:50:35 -0500

    Working on getting token from Ghost

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-; 
+var request = require('superagent');
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Blog URL: ", function(url) {
+	rl.question("Email: ", function(email) {
+		rl.question("Password: ", function(password) {
+	request.post(url + '/ghost/api/v0.1/authentication/token/')
+            .send({grant_type: 'password', username: email, password: password, client_id: 'ghost-admin'})
+            .end(function (err, res) {
+                if (err) {
+		    console.log("ERROR");
+                    console.log(err);
+                }
+		else {
+		console.log(res.body);
+		console.log(res.body.access_token);
+		}
+		rl.close();
+            });
+		});
+	});
+});
